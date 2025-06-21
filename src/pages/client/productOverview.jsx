63e16beafc480c09e057e0310b9547ef4ac26868
KokilaSandamini <sandaminikokila315@gmail.com>
2025-06-21 23:55:00 +0530

    Day 17

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Loader from "../../components/loader"
 import toast from "react-hot-toast"
+import ImageSlider from "../../components/imageSlider"
+import getCart, { addToCart } from "../../utils/cart"
 
 export default function ProductOverview() {
     const params = useParams()
@@ -21,7 +23,7 @@ export default function ProductOverview() {
                 axios.get(import.meta.env.VITE_BACKEND_URL + "/api/product/" + params.id).then(
                     (res) => {
                         console.log(res)
-                        setProduct(res.data)
+                        setProduct(res.data.product)
                         setStatus("loaded")
                     }
                 ).catch(
@@ -40,8 +42,48 @@ export default function ProductOverview() {
             }
             {
                 status == "loaded" &&
-                <div className="w-full h-full">
-                    product loaded
+                <div className="w-full h-full flex">
+                    <div className="w-[50%] h-full ">
+                        {console.log(product)}
+                        <ImageSlider images={product.images} />
+                    </div>
+                    <div className="w-[50%] h-full bg-blue-100 p-[40px]">
+                        <h1 className="text-3xl font-bold text-center mb-[40px]">{product.name}{" | "}<span className="text-2xl font-semibold text-center text-gray-500">{product.altNames.join(" | ")}
+                            </span></h1>
+                        
+                            
+                        
+                        <div className="w-full flex justify-center mb-[40px]">
+                            {product.labeledPrice > product.price ? (
+                                <>
+                                    <h2 className="text-2xl mr-[20px]">LKR: {product.price.toFixed(2)}</h2>
+                                    <h2 className="text-2xl line-through text-gray-500">
+                                        LKR: {product.labeledPrice.toFixed(2)}
+                                    </h2>
+                                </>
+                            ) : (
+                                <h2 className="text-2xl mr-[20px]">{product.price}</h2>
+                            )}
+                        </div>
+                        <p className="text-xl text-center text-gray-500 mb-[40px]">
+                            {product.description}
+                        </p>
+                        <div className="w-full flex justify-center mb-[40px]">
+                            <button className="bg-pink-800 border cursor-pointer border-pink-800 text-white w-[200px] h-[50px] rounded-lg hover:bg-white hover:text-pink-800 transition-all duration-300 ease-in-out"
+                            onClick={
+                                ()=>{
+                                    addToCart(product, 1)
+                                    toast.success("Product added to cart")
+                                    console.log(getCart())
+                                }
+                                }>
+                                Add to Cart
+                            </button>
+                            <button className="bg-pink-800 border cursor-pointer border-pink-800 text-white w-[200px] h-[50px] rounded-lg hover:bg-white hover:text-pink-800 transition-all duration-300 ease-in-out ml-[20px]">
+                                Buy Now
+                            </button>
+                            </div>
+                    </div>
                 </div>
             }
             {
